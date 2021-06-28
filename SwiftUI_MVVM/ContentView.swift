a7c46784c795642a327055327ee9531b70e85635
ZolotAlice <zolotalice@gmail.com>
2021-06-28 17:32:55 +0300

    first commit

--- SwiftUI_MVVM/ContentView.swift
+++ SwiftUI_MVVM/ContentView.swift
@@ -9,72 +9,48 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    
+    @StateObject private var taskListVM = TaskListViewModel()
+    
+    func deleteTask(at offsets: IndexSet){
+        offsets.forEach{ index in
+            let task = taskListVM.tasks[index]
+            taskListVM.delete(task)
+        }
+        taskListVM.getAllTasks()
+    }
+    
     var body: some View {
-        List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+        VStack{
+            HStack{
+            TextField("Enter task name", text: $taskListVM.title)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                Button("Save"){
+                    taskListVM.save()
+                    taskListVM.getAllTasks()
+                    taskListVM.title = ""
+                }
             }
-            .onDelete(perform: deleteItems)
-        }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
-
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
+            
+            List{
+                ForEach(taskListVM.tasks, id: \.id){ task in
+                    Text(task.title)
+                }.onDelete(perform: deleteTask)
             }
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+            
+            Spacer()
+        }.padding()
+        .onAppear(perform: {
+            taskListVM.getAllTasks()
+        })
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
---
+++ SwiftUI_MVVM/View Models/TaskListViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  TaskListViewModel.swift
+//  SwiftUI_MVVM
+//
+//  Created by Alice Zolotareva on 28.06.2021.
+//
+
+import Foundation
+import CoreData
+
+class TaskListViewModel: ObservableObject{
+    
+    var title: String = ""
+    @Published var tasks: [TaskViewModel] = []
+    
+    func getAllTasks() {
+        tasks = CoreDataManager.shared.getAllTasks().map(TaskViewModel.init)
+    }
+    
+    func delete(_ task: TaskViewModel){
+        let existingTask = CoreDataManager.shared.getTaskById(id: task.id)
+        if let existingTask = existingTask{
+        CoreDataManager.shared.deleteTask(task: existingTask)
+        }
+    }
+    
+    func save(){
+        
+        let task = Task(context: CoreDataManager.shared.viewContext)
+        task.title = title
+        
+        CoreDataManager.shared.save()
+    }
+    
+}
+
+struct TaskViewModel {
+    
+    let task: Task
+    
+    var id: NSManagedObjectID {
+        return task.objectID
+    }
+    var title: String {
+        return task.title ?? ""
+    }
+}
